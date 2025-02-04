fd649441d85bff6b095acb760dd9a94bf806f90f
Miroslav Bauer <mirekys@gmail.com>
2025-02-04 14:28:17 +0100

    feat: initialize repo infrastructure

---
+++ src/index.js
@@ -0,0 +1,8 @@
+/*
+ * This file is part of Invenio-Files-Uppy-Js.
+ * Copyright (C) 2025 CERN.
+ * Copyright (C) 2025 CESNET.
+ *
+ * Invenio-Files-Uppy-Js is free software; you can redistribute it and/or modify it
+ * under the terms of the MIT License; see LICENSE file for more details.
+ */
